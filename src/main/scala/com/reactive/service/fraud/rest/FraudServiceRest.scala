5fb327413fa964b9a79618728962157c45c4b6dc
naspredam <mnaspreda@localhost.localdomain>
2017-06-01 22:01:18 +0200

    Correct tab object

--- src/main/scala/com/reactive/service/fraud/rest/FraudServiceRest.scala
+++ src/main/scala/com/reactive/service/fraud/rest/FraudServiceRest.scala
@@ -21,21 +21,21 @@ object FraudServiceRest extends App {
   implicit val system = ActorSystem()
   implicit val executor = system.dispatcher
   implicit val materializer = ActorMaterializer()
-
+  
   val config = ConfigFactory.load()
   val logger = Logging(system, getClass)
   
   // create the result listener, which will print the result and 
-	// shutdown the system
-	val listener = system.actorOf(Props[FraudNotificatorActor], name = "fraud-notification-listener")
-
-	// create the master
-	val master = system.actorOf(Props(classOf[MasterFraudAnalyzerActor], listener), name = "master")
-	
-	val api = system.actorOf(Props(classOf[FraudServiceRestActor], master), "api-actor")
-	
-	val host = config.getString("http.interface")
+  // shutdown the system
+  val listener = system.actorOf(Props[FraudNotificatorActor], name = "fraud-notification-listener")
+  
+  // create the master
+  val master = system.actorOf(Props(classOf[MasterFraudAnalyzerActor], listener), name = "master")
+  
+  val api = system.actorOf(Props(classOf[FraudServiceRestActor], master), "api-actor")
+  
+  val host = config.getString("http.interface")
   val port = config.getInt("http.port")
-	IO(Http) ! Http.Bind(listener = api, interface = host, port = port)
+  IO(Http) ! Http.Bind(listener = api, interface = host, port = port)
 
 }
