4eca67e892ee3d61ad9e6e87a37afd651762edb3
naspredam <mnaspreda@localhost.localdomain>
2017-06-01 22:04:11 +0200

    Correct tabulator

--- src/main/scala/com/reactive/service/fraud/actor/FraudNotificatorActor.scala
+++ src/main/scala/com/reactive/service/fraud/actor/FraudNotificatorActor.scala
@@ -5,8 +5,8 @@ import com.reactive.service.fraud.core.ResultFraudMessage
 
 class FraudNotificatorActor extends Actor {
   
-	def receive = {
-	case ResultFraudMessage(op, response) =>
-    	println("\n\tDetectado operativa de fraude!!! id=%s -- %s".format(op.id, op toString))
-	}
+  def receive = {
+  case ResultFraudMessage(op, response) =>
+    println("\n\tDetectado operativa de fraude!!! id=%s -- %s".format(op.id, op toString))
+  }
 }
